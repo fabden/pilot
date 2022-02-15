40378fb603dbae2a01b4a079c2d96dc8a841825d
FabDen <fabden@gmail.com>
2022-02-15 16:00:28 +0100

    creation des componant ok

---
+++ frontend/src/Compoment/ListesJoueur/CardJoueur.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CardJoueur() {
+  return (
+    <div>CardJoueur</div>
+  )
+}
+
+export default CardJoueur
--- frontend/src/Compoment/ListesJoueur/index.js
+++ frontend/src/Compoment/ListesJoueur/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import axios from 'axios'
 
 function ListeJoueur() {
+
+
+  
   return (
     <div>ListeJoueur</div>
   )
